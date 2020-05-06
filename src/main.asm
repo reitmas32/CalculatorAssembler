96cc5be0a33f33ff8c3639da002c178fcfddcb7b
reitmas32 <rafa.zamora.rals@gmail.com>
2020-05-06 10:31:26 -0500

    Creacion del main

---
+++ src/main.asm
@@ -0,0 +1,16 @@
+title "Proyecto para EyPC 2020-2 Calculadora de dos numeros con Graficos"
+	.model small
+	.386
+	.stack 64
+	.data
+.code				;segmento de codigo
+inicio:					;etiqueta inicio
+	mov ax,@data 		;AX = directiva @data, @data es una variable de sistema que contiene la direccion del segmento de datos 
+	mov ds,ax 			;DS = AX, inicializa segmento de datos
+	mov ax, 0Ah
+salir:					;inicia etiqueta salir
+	mov ah,4Ch			;AH = 4Ch, opcion para terminar programa
+	mov al,0			;AL = 0 Exit Code, codigo devuelto al finalizar el programa
+						;AX es un argumento necesario para interrupciones
+	int 21h				;senal 21h de interrupcion, pasa el control al sistema operativo
+	end inicio			;fin de etiqueta inicio, fin de programa
